0625bcafb9ca6980bde41065cfa2fa50de526c01
Michal <michal@MichalPC.(none)>
2013-12-08 22:35:48 +0100

    Some JS

--- WEDT/wedt/wedt/static/utils.js
+++ WEDT/wedt/wedt/static/utils.js
@@ -1,7 +1,30 @@
+function windowReady()
+{
+	var pxPageWidth = $(window).width();
+	var fontPageWidth = Math.floor(pxPageWidth * 0.9);
+	
+	$("input[name=question]").css("width", fontPageWidth);
+	
+	$("#info").text("");
+}
+
 function resizeQuestionInput()
 {
 	var pxPageWidth = $(window).width();
 	var fontPageWidth = Math.floor(pxPageWidth * 0.9);
 	
 	$("input[name=question]").css("width", fontPageWidth);
+}
+
+function validateQuestion() {
+	var question = $("#questionForm").find('input[name="question"]').val();
+	if (question == "")
+	{
+		$("#info").text("Input field cannot be empty.");
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
