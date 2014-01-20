77857d8546d67a5cf75459607d76741564149da0
Jan Grzegorowski <grzegorowski@gmail.com>
2014-01-20 16:57:35 +0100

    add: URL validation, add: alert box

--- WEDT/wedt/wedt/static/utils.js
+++ WEDT/wedt/wedt/static/utils.js
@@ -1,30 +1,21 @@
-function windowReady()
-{
-	var pxPageWidth = $(window).width();
-	var fontPageWidth = Math.floor(pxPageWidth * 0.9);
-	
-	$("input[name=question]").css("width", fontPageWidth);
-	
-	$("#info").text("");
-}
+// initialize foundation
+$(document).foundation();
 
-function resizeQuestionInput()
-{
-	var pxPageWidth = $(window).width();
-	var fontPageWidth = Math.floor(pxPageWidth * 0.9);
-	
-	$("input[name=question]").css("width", fontPageWidth);
-}
+// add close button to dropdown
+$('#howto-close').click(function() { $('body').trigger('click')});
 
 function validateQuestion() {
-	var question = $("#questionForm").find('input[name="question"]').val();
-	if (question == "")
-	{
-		$("#info").text("Input field cannot be empty.");
-		return false;
-	}
-	else
-	{
+  // url validation RegExp
+  var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  var message = 'Provided URL seems to be invalid, please check it once more';
+  var question = $("#questionForm").find('input[name="question"]').val();
+
+	if (regexp.test(question)) {
 		return true;
-	}
+  }
+  else {
+    $('#alert').html('<div class="alert-box alert" data-alert><span id="info">' + message + '</span><a class="close" href="#">&times;</a></div>');
+    $(document).foundation('alert');
+    return false;
+  }
 }
