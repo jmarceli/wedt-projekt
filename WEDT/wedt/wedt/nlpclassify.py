1f9a71f5d4ea4a67b708b34fe2a304a5c3d72f76
aratajczak <andrratajczak@gmail.com>
2014-01-21 21:59:44 +0100

    some documentation

--- WEDT/wedt/wedt/nlpclassify.py
+++ WEDT/wedt/wedt/nlpclassify.py
@@ -3,11 +3,13 @@ import os, pickle, nltk, re
 classifier_path = os.path.join("wedt","classifiers")
 
 def choose_answers(classifier_name, topic):
+	"""Performs the classification of posts in the topic using given classifier"""
 	classifier,features = load_classifier(classifier_name)
 	classes = classifier.batch_classify(map(features, ((topic, t) for t in topic)))
 	return [post[0] for post in zip(topic, classes) if post[1]=="acc"]
 
 def load_classifier(name):
+	"""Loads a classifier with a given name from the "classifiers" folder"""
 	with open(os.path.join(classifier_path, name), 'r') as file:
 		classifier, features = pickle.load(file)
 	return classifier, features
