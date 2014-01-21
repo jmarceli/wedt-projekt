f8e87e333e3e8c93e117e670cbe0ed4312304bd0
aratajczak <andrratajczak@gmail.com>
2014-01-21 16:24:58 +0100

    check option for train.py (doesn't like unicodes)

--- WEDT/wedt/train.py
+++ WEDT/wedt/train.py
@@ -16,7 +16,7 @@ def main(argv):
 				addresslist = file.read().splitlines()
 				wedt.training.gather_topics(addresslist, argv[2])
 				
-	if argv[1] == "train":
+	elif argv[1] == "train":
 		if len(argv) < 6:
 			print_help()
 			return
@@ -25,7 +25,18 @@ def main(argv):
 			wedt.training.train_classifier(argv[3], argv[5], feature, argv[2])
 		except AttributeError:
 			print "Wrong feature function name - see wedt/features.py"
-		
+	
+	elif argv[1] == "check":
+		if len(argv) < 4:
+			print_help()
+			return
+		from wedt.tparser import parse
+		from wedt.nlpclassify import choose_answers
+		answers = choose_answers(argv[2], parse(argv[3]))
+		for answer in answers:
+			print answer
+	else:
+		print_help()
 		
 		
 def print_help():
@@ -37,10 +48,16 @@ train.py gather directory [filenames...]
 
 train.py train name type feature trainset
 	Trains a new classifier.
-	name - name for the new classifier
-	type - classifier type: MaxEnt, NaiveBayes or PositiveNaiveBayes
-	feature - featureset name (as in wedt/features.py)
-	trainset - name of the directory containing the training topics"""
+	name		name for the new classifier
+	type		classifier type: MaxEnt, NaiveBayes or PositiveNaiveBayes
+	feature		featureset name (as in wedt/features.py)
+	trainset	name of the directory containing the training topics
+
+train.py check classifier address
+	Chooses an answer based on given classifier
+	classifier	classifier name
+	address		topic address
+"""
 
 if __name__ == "__main__":
 	if len(sys.argv) == 1:
