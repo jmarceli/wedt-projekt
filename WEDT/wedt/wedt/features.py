91a0dac6d7a00e3c9ab14935a5b827fa9c1c4cac
aratajczak <andrratajczak@gmail.com>
2014-01-21 13:43:00 +0100

    some more features and a sample featureset

--- WEDT/wedt/wedt/features.py
+++ WEDT/wedt/wedt/features.py
@@ -1,6 +1,13 @@
 import nltk, re
 
-def simple_features((topic, post)):
+def featureset_sample(posts):
+	features = {}
+	features.update(longer_than_op(posts))
+	features.update(author_mentioned(posts))
+	features.update(author_thanked(posts))
+	return features
+
+def longer_than_op((topic, post)):
 	features = {}
 	features['longer-than-op'] = len(post.text) > len(topic.op.text)
 	return features
@@ -11,4 +18,16 @@ def word_features((topic, post)):
 		features[word] = True
 	return features
 
-
+def author_mentioned((topic, post)):
+	features = {"author-mentioned":False}
+	for p in topic:
+		if re.search(re.escape(post.author), p.text):
+			features["author-mentioned"] = True
+	return features
+	
+def author_thanked((topic, post)):
+	features = {"author-thanked":False}
+	for p in topic:
+		if re.search("[Tt]hanks? (you)? "+post.author, p.text):
+			features["author-thanked"] = True
+	return features
